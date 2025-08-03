592926004de83827f80b92a1c744f75fc734be88
1R10 <ryanguilherme2003@gmail.com>
2025-08-03 09:07:45 -0300

    q3 quase pronta

--- Unidade02- Avaliacao01/q3.py
+++ Unidade02- Avaliacao01/q3.py
@@ -2,23 +2,32 @@
 # =====================================================================================================
 # Checklist
 #
-#   Lista....................NOK
 #   Tratamento de erros......NOK
-#     --> Tamanho............NOK
+#     --> Tamanho.............OK
 #     --> Palavra válida.....NOK
-#     --> Tentativas.........NOK...?
-#   Cores.................... OK
-#   Tratamento de cores...... OK
-#   Encerramento por pontos..NOK
+#     --> Tentativas..........OK
+#   Cores.....................OK
+#   Tratamento de cores.......OK
+#   Encerramento..............OK
 
 
 # =====================================================================================================
 # Parte de gerenciamento de palavras
 
-import random
+import random #, requests
+    
+'''sites_palavras = ( # Sites, do próprio termo, para validar as palavras.
+                    'https://raw.githubusercontent.com/fserb/pt-br/refs/heads/master/palavras'             ,
+                    'https://raw.githubusercontent.com/fserb/pt-br/refs/heads/master/conjuga%C3%A7%C3%B5es',
+                    'https://github.com/fserb/pt-br/raw/refs/heads/master/verbos'                          ,
+                    'https://github.com/fserb/pt-br/raw/refs/heads/master/paises'                          ,
+                    'https://github.com/fserb/pt-br/raw/refs/heads/master/municipios'                      ,
+                    'https://github.com/fserb/pt-br/raw/refs/heads/master/estados'                         ,
+                    'https://github.com/fserb/pt-br/raw/refs/heads/master/dicio'                           ,
+                    'https://github.com/fserb/pt-br/raw/refs/heads/master/data'                            ,
+                    'https://github.com/fserb/pt-br/raw/refs/heads/master/continentes'                      )'''
 
-
-palavras = ( 
+palavras         = ( # Palavras do jogo
     "ADAGA", "ADUBO", "AMIGO", "ANEXO", "ARAME", "ARARA", "ARROZ",
     "ASILO", "ASTRO", "BAILE", "BAIXA", "BALAO", "BALSA", "BARCO",
     "BARRO", "BEIJO", "BICHO", "BORDA", "BORRA", "BRAVO", "BREJO",
@@ -46,8 +55,9 @@ segredo_1 = palavras[random.randint(0,len(palavras))] # Escolhe do index 0 ao fi
 segredo_2 = palavras[random.randint(0,len(palavras))]
 
 while segredo_2 == segredo_1: # Caso as palavras sejam iguais, é gerado um looping até não serem mais.
-    segredo_1 = palavras[random.randint(0,len(palavras))]
+    segredo_2 = palavras[random.randint(0,len(palavras))]
 print(segredo_1, segredo_2)
+
 
 # =====================================================================================================
 # Cores:
@@ -59,7 +69,7 @@ padrao     = '\033[0m'  # Resetar
 
 
 
-def formatar(palavra):
+def formatar(palavra): # Puxa cdaa lista que vai ser criada com a palavra e cor e formata num print/for.
     for fundo, letra, padrao in palavra:
         print(f'{fundo}{letra}{padrao}', end='')
     
@@ -69,11 +79,12 @@ def formatar(palavra):
 
 tentativas = 0
 
-d_tentativas = {1:'Impossível',
-                2:'Ninja',
+d_tentativas = { # Dict para formatar a mensagem final com base na quantidade de tentativas.
+                1:'Impossível'    ,
+                2:'Ninja'         ,
                 3:'Impressionante',
-                4:'Interessante',
-                5:'Pode melhorar',
+                4:'Interessante'  ,
+                5:'Pode melhorar' ,
                 6: 'Foi por pouco',
                 7:f'Palavras: {segredo_1}, {segredo_2}'}
 
@@ -81,20 +92,24 @@ d_tentativas = {1:'Impossível',
 # =====================================================================================================
 # Parte principal:
 
-l_segredos = []
+l_resposta = [[segredo_1], [segredo_2]]
+l_segredos = [[], []]
+
 while tentativas != 7:
     l_palavra = [[],[]] # resetar toda vez para a palavra não entrar na outra lista novamente.
     while True:
-        termo = (input('Escreva uma palavra de 5 dígitos: ')).upper() # Pede o termo.
-        if len(termo) == 5 and termo.isalpha() and termo in palavras:
+        termo = (input('Escreva uma palavra de 5 letras: ')).upper() # Pede o termo.
+        if len(termo) == 5 and termo.isalpha():
             break
         else:
             print('Termo inválido.')
 
-        
+
+    tentativas += 1 # Palavra válida = tentativa gasta.
     
 #   --------------------------------------------------------------------------------
-    # Vai comparar letra por letra e salvar numa lista.
+    # Vai comparar letra por letra, colorir e salvar numa lista (cada palavra).
+
     for let in range(len(termo)): 
         if termo[let] == segredo_1[let]:
             l_palavra[0].append((f_verde, termo[let], padrao))
@@ -114,75 +129,26 @@ while tentativas != 7:
         elif termo[let] in segredo_2:
             l_palavra[1].append((f_amarelo, termo[let], padrao))
 
-        elif termo[let] not in segredo_2[let]:
+        else:
             l_palavra[1].append((f_preto, termo[let], padrao))
 #   ---------------------------------------------------------------------------------    
+#   Print formatado no def na aba de cores.    
     formatar(l_palavra[0])
     print(' ', end='')
     formatar(l_palavra[1])
     print('\n'*2)
 
+# ------------------------------------------------------------------------------------
+#   condição de vitória: se as listas forem iguais o jogo acaba.
+    if termo == segredo_1:
+        l_segredos[0].append(termo)
+    if termo == segredo_2:
+        l_segredos[1].append(segredo_2)
+    if l_segredos == l_resposta:
+        break
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    tentativas += 1
-print(f'{d_tentativas[tentativas]}')
+print(f'\033[44m{d_tentativas[tentativas]} {padrao}')
 
 
 
 # =====================================================================================================
-'''
-
-[('\x1b[42m', 'G', '\x1b[0m'),
-('\x1b[42m', 'E', '\x1b[0m'),
-('\x1b[42m', 'N', '\x1b[0m'), 
-('\x1b[42m', 'I', '\x1b[0m'),
-('\x1b[42m', 'O', '\x1b[0m')] 
-- 
-[('\x1b[40m', 'G', '\x1b[0m'),
-('\x1b[42m', 'E', '\x1b[0m'), 
-('\x1b[42m', 'N', '\x1b[0m'), 
-('\x1b[40m', 'I', '\x1b[0m'), 
-('\x1b[42m', 'O', '\x1b[0m')]
-
-'''
