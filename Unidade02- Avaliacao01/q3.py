f147e4f95da93b254ac86ec050b339bdd7dcd1a1
1R10 <ryanguilherme2003@gmail.com>
2025-08-03 07:35:37 -0300

    commit apenas para salvar

--- Unidade02- Avaliacao01/q3.py
+++ Unidade02- Avaliacao01/q3.py
@@ -2,13 +2,15 @@
 # =====================================================================================================
 # Checklist
 #
-#   Lista.....................OK
+#   Lista....................NOK
 #   Tratamento de erros......NOK
-#   Cores....................NOK
-#   Tratamento de cores......NOK
+#     --> Tamanho............NOK
+#     --> Palavra válida.....NOK
+#     --> Tentativas.........NOK...?
+#   Cores.................... OK
+#   Tratamento de cores...... OK
+#   Encerramento por pontos..NOK
 
-def rodou():
-    print('Código rodando...')
 
 # =====================================================================================================
 # Parte de gerenciamento de palavras
@@ -45,10 +47,10 @@ segredo_2 = palavras[random.randint(0,len(palavras))]
 
 while segredo_2 == segredo_1: # Caso as palavras sejam iguais, é gerado um looping até não serem mais.
     segredo_1 = palavras[random.randint(0,len(palavras))]
-print(segredo_1)
+print(segredo_1, segredo_2)
 
 # =====================================================================================================
-# Cores
+# Cores:
 
 f_verde    = '\033[42m' # Verde   = Letra certa, lugar certo
 f_amarelo  = '\033[43m' # Amarelo = Letra certa, lugar errado
@@ -56,32 +58,131 @@ f_preto    = '\033[40m' # Preto   = Letra não existente
 padrao     = '\033[0m'  # Resetar
 
 
+
+def formatar(palavra):
+    for fundo, letra, padrao in palavra:
+        print(f'{fundo}{letra}{padrao}', end='')
+    
+
 # =====================================================================================================
-# Parte principal.
-rodou()
+# Tentativas:
 
-tentativas = 7
+tentativas = 0
 
-while tentativas > 0:
+d_tentativas = {1:'Impossível',
+                2:'Ninja',
+                3:'Impressionante',
+                4:'Interessante',
+                5:'Pode melhorar',
+                6: 'Foi por pouco',
+                7:f'Palavras: {segredo_1}, {segredo_2}'}
+
+
+# =====================================================================================================
+# Parte principal:
+
+l_segredos = []
+while tentativas != 7:
+    l_palavra = [[],[]] # resetar toda vez para a palavra não entrar na outra lista novamente.
+    while True:
+        termo = (input('Escreva uma palavra de 5 dígitos: ')).upper() # Pede o termo.
+        if len(termo) == 5 and termo.isalpha() and termo in palavras:
+            break
+        else:
+            print('Termo inválido.')
+
+        
     
-    termo = (input('Escreva uma palavra de 5 dígitos: ')).upper() # Pede o termo.
-    
-    #l_palavras = [[],[]]
-
-    for let in range(len(termo)):
+#   --------------------------------------------------------------------------------
+    # Vai comparar letra por letra e salvar numa lista.
+    for let in range(len(termo)): 
         if termo[let] == segredo_1[let]:
-            print(f_verde, termo[let], padrao, end='')
+            l_palavra[0].append((f_verde, termo[let], padrao))
 
         elif termo[let] in segredo_1:
-            print(f_amarelo, termo[let], padrao, end='')
+            l_palavra[0].append((f_amarelo, termo[let], padrao))
 
         else:
-            print(f_preto, termo[let], padrao, end='')
+            l_palavra[0].append((f_preto, termo[let], padrao))
+
+    print(' ', end='')
+
+    for let in range(len(termo)):
+        if termo[let] == segredo_2[let]:
+            l_palavra[1].append((f_verde, termo[let], padrao))
+
+        elif termo[let] in segredo_2:
+            l_palavra[1].append((f_amarelo, termo[let], padrao))
+
+        elif termo[let] not in segredo_2[let]:
+            l_palavra[1].append((f_preto, termo[let], padrao))
+#   ---------------------------------------------------------------------------------    
+    formatar(l_palavra[0])
+    print(' ', end='')
+    formatar(l_palavra[1])
+    print('\n'*2)
 
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    tentativas += 1
+print(f'{d_tentativas[tentativas]}')
 
 
 
 # =====================================================================================================
+'''
+
+[('\x1b[42m', 'G', '\x1b[0m'),
+('\x1b[42m', 'E', '\x1b[0m'),
+('\x1b[42m', 'N', '\x1b[0m'), 
+('\x1b[42m', 'I', '\x1b[0m'),
+('\x1b[42m', 'O', '\x1b[0m')] 
+- 
+[('\x1b[40m', 'G', '\x1b[0m'),
+('\x1b[42m', 'E', '\x1b[0m'), 
+('\x1b[42m', 'N', '\x1b[0m'), 
+('\x1b[40m', 'I', '\x1b[0m'), 
+('\x1b[42m', 'O', '\x1b[0m')]
+
+'''
