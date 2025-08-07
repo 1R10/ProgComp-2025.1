78f559796faa5345e38beb893f5be242e6a0fc9f
1R10 <ryanguilherme2003@gmail.com>
2025-08-06 23:59:39 -0300

    q2

--- Unidade02- Avaliacao01/q2.py
+++ Unidade02- Avaliacao01/q2.py
@@ -1,22 +1,36 @@
 # Aluno: Ryan Guilherme Costa de Moura; 20242014050039
-# =====================================================================================================
-# Palíndromo 10 --> 100000
+#ALUNO: Roberto Araujo Batista, Matricula: 20251014050041
+#PROFESSOR: Galileu Batista, CURSO: redes de computadores 2025.1, IFRN
 
 
-contador = 0
+'''
+2) Faça um programa que conta quantos números palíndromos existem entre
+10 e 100000. Um número é palíndromo quando tem o mesmo valor se lido da
+esquerda para a direita ou da direita para a esquerda. Ex: 98189 é palíndromo. Não
+use strings na sua resposta.
+'''
 
-for numero in range(10, 100001): # Conjunto -- > for tem melhor uso.
-    numero_temp = numero  # Cópia temporária para manter o valor original.
-    numero_invertido = 0
+
+def identificar_palindromo(numero):
+    novonumero = numero
+    numeroInverso = 0
+    while novonumero != 0:
+        algarismo = novonumero % 10
+        novonumero = novonumero //10
+        numeroInverso = (numeroInverso *10) + algarismo
+    if numeroInverso == numero:
+        print(f'{numero} é palindromo')
+        return True
+    else:
+        print(f'{numero} não é palindromo')
+        return False
 
 
- # Pega o último número de um e adiciona no final do outro (revertendo - o)
-    while numero_temp > 0: # vai até ter algum valor na variavel 
-        digito = numero_temp % 10 
-        numero_invertido = numero_invertido * 10 + digito
-        numero_temp = numero_temp // 10
+palindromos = 0
+#o programa irá chamar a função de identificar palindromo no intervalo do range:
+for num in range(10,100001):
+    #a função retorna true para palindromo, então é só contar a quantidade de True's retornados
+    if identificar_palindromo(num):
+        palindromos = palindromos +1
 
-    if numero == numero_invertido:
-        contador += 1
-
-print(f'A quantidade de palíndromos entre 10 e 100000 é {contador}.')
+print(f'A quantidade de palindromos entre o intervalo de 10 e 100000 é: {palindromos}')
